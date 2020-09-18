e8b3df2a3f772b4486bcf0a0748a015ebff51d07
kim <kimqadasha@gmail.com>
2020-09-18 14:05:44 +0700

    autoinstall v2

--- autografana.sh
+++ autografana.sh
@@ -4,10 +4,10 @@ yum update -y  # To get the latest package lists
 yum install wget vim nano screen net-tools # install requirment
 cd /tmp # chang dir to /tmp
 wget https://dl.grafana.com/oss/release/grafana-7.1.5-1.x86_64.rpm # get rpm file garafana
-yum install grafana-7.1.5-1.x86_64.rpm # install grafana
-yum install fontconfig #Install additional font packages
-yum install freetype* #Install additional font packages
-yum install urw-fonts #Install additional font packages
+yum install grafana-7.1.5-1.x86_64.rpm -y # install grafana
+yum install fontconfig -y #Install additional font packages
+yum install freetype* -y #Install additional font packages
+yum install urw-fonts -y #Install additional font packages
 systemctl start grafana-server
 systemctl restart grafana-server
 systemctl enable grafana-server.service
